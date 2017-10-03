046cb43e5488226932ded2d76bd80f8d31a286a1
rishavchauhan <rishav2204@gmail.com>
2017-10-03 23:17:33 +0530

    Add files via upload

---
+++ stringtoobject.java
@@ -0,0 +1,14 @@
+package stringtoobject;
+
+class a
+{
+	int b=10;
+}
+public class stringtoobject {
+public static void main(String args[])
+{
+	String arg="a A new a()";
+	String words[]=arg.split("
+	System.out.println("b="+obj.b);
+}
+}
